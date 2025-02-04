818c0ee23d8bc9c761db9fc381315ca0ba1beea4
Fofana Moussa <moussa.fofana@cgi.com>
2025-02-04 09:03:06 +0100

    - herline +lualine

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,6 +1,17 @@
 return {
 	"xiyaowong/transparent.nvim",
 	config = function()
-		require("transparent").setup()
+		require("transparent").setup({
+			extra_groups = {
+				"lualine_z_normal",
+				"lualine_c_normal",
+				"lualine_b_normal",
+				"lualine_c_12",
+				"lualine_z_16",
+				"lualine_x_2",
+				"lualine_c_9",
+				"lualine_b_4"
+			},
+		})
 	end
 }
